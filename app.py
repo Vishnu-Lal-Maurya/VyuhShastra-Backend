eeb15a451081fe6ac57ded83dc49998cfaceeb05
Vishnu-Lal-Maurya <vishnulal.maurya.cse22@itbhu.ac.in>
2024-12-25 11:32:00 +0530

    updated repo

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api')
 def hello_world():
     return "Hello from Flask!"
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
